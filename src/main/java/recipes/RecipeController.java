c27d2aa66db8fbf575495d0a49bd25fe16d108f3
Jayson Lorenzen <jayson_lorenzen@hotmail.com>
2020-08-20 07:20:44 -0700

    adding the properties file

--- src/main/java/recipes/RecipeController.java
+++ src/main/java/recipes/RecipeController.java
@@ -5,20 +5,53 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.http.HttpStatus;
+import java.util.Properties;
+import java.util.Set;
 
 @RestController
 public class RecipeController {
-    private static final Recipe[] recipes = {
-            new Recipe(1L, "Pickled Peppers", "TODO ..."),
-            new Recipe(2L, "Cold Sesame Soba", "TODO ...")};
+
+// TO HOLD SOME YUMMY RECIPES
+private Recipe[] recipes;
+
+    //
+    public RecipeController() {
+     this.recipes = this.loadRecipies();
+    }
 
 
+    // Reads a properties file of recipes and returns an array of recipe objects
+    private Recipe[] loadRecipies() {
+      String propFile = "recipes.properties";
+      Recipe[] recipeArray = null;
+      try {
+        Properties props = new Properties();
+        props.load(RecipeController.class.getResourceAsStream("/" + propFile));
+        Set<String> keySet = props.stringPropertyNames();
+        int keySetLen = keySet.size();
+        recipeArray = new Recipe[keySetLen];
+        int i = 0;
+        for (String keyStr : keySet) {
+           recipeArray[i] = new Recipe((long)i, keyStr, props.getProperty(keyStr));
+           i++;
+        }
+      }
+      catch(Exception e){
+        System.out.println("Error encountered while loading/processing properties file: " + propFile);
+        e.printStackTrace();
+      }
+      return recipeArray;
+    }
+
+
+    //
     @GetMapping("/recipes")
     public Recipe[] recipes(){
         return this.recipes;
     }
 
 
+    //
     @GetMapping("/recipes/id/{id}")
     public Recipe recipe(@PathVariable int id) {
         for (Recipe recipe: recipes) {
@@ -32,7 +65,8 @@ public class RecipeController {
     }
 
 
-    @GetMapping("/recipies/title/{title}")
+    //
+    @GetMapping("/recipes/title/{title}")
     public Recipe recipe(@PathVariable String title) {
         for (Recipe recipe: recipes) {
             if (recipe.getTitle().equals(title)) {
@@ -43,5 +77,4 @@ public class RecipeController {
                 HttpStatus.NOT_FOUND, "Recipe not found"
         );
     }
-
 }
